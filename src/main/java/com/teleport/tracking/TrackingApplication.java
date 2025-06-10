aa16c939598156cb80c9a8c9c60a907815a08e7e
priteshbhate <bhatepritesh@gmail.com>
2025-06-10 21:24:42 +0530

    REDIS config changes

--- src/main/java/com/teleport/tracking/TrackingApplication.java
+++ src/main/java/com/teleport/tracking/TrackingApplication.java
@@ -2,12 +2,16 @@ package com.teleport.tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class TrackingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrackingApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(TrackingApplication.class, args);
+		System.out.println("Active profile(s): " + Arrays.toString(context.getEnvironment().getActiveProfiles()));
 	}
 
 }
